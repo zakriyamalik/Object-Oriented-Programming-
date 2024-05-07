aabded824d4dd25715cbe516e30e743f7e8c1026
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-05-08 00:13:15 +0500

    Add files via upload

---
+++ Source(1).cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//using namespace std;
+//template <class type>
+//type surprise(type x, type y)
+//{
+//	return x + y;
+//}
+//int main()
+//{
+//	cout << surprise(5, 7) << endl;
+//	string str1 = "Sunny";
+//	string str2 = " Day";
+//	cout << surprise(str1, str2) << endl;
+//}
+///*12
+//Sunny Day
+//*/
